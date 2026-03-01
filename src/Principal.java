995e6d4526a64e2423355fef8cfb83bd59a6e577
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-03-01 15:52:43 -0300

    criado entrada leitura do tipo Scanner para o usuário inserir o cep desejado na pesquisa

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
 
+        Scanner leitura =  new Scanner(System.in);
         ConsultarCep consultarCep = new ConsultarCep();
-        Endereco novoEndereco =  consultarCep.buscarEndereco("60420470");
-        System.out.println(novoEndereco);
+
+        System.out.println("Digite o número do CEP desejado para consulta:");
+        var cep = leitura.nextLine();
+
+        try{
+            Endereco novoEndereco =  consultarCep.buscarEndereco(cep);
+            System.out.println(novoEndereco);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Aplicação será finalizada");
+        }
+
+
     }
 }
